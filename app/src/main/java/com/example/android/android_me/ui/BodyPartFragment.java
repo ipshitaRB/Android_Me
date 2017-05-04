fa71db65a4bcae59c2d3dd3a6409625ae3a3c010
Ipshita <iroyburman89@gmail.com>
2017-05-03 17:07:11 -0700

    Created Fragment ImageView and BodyPartFragment class

---
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -0,0 +1,10 @@
+package com.example.android.android_me.ui;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Ipshita on 03-05-2017.
+ */
+
+public class BodyPartFragment extends Fragment {
+}
